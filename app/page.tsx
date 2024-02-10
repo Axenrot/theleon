43f6a72e434e5c57cb7d3175a6aca7fc4b11996c
Leon <yurileon@hotmail.com.br>
2024-02-10 18:23:15 -0300

    title fix

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center h-full min-h-screen overflow-hidden screen-container">
+    <div className="flex flex-col items-center justify-center h-full min-h-screen overflow-hidden border border-red-500 screen-container">
       <span className="fixed top-0 left-0 w-full h-full -z-40 bg-gradient-to-tl from-white via-white/10 to-white" />
       <Particles
         className="fixed inset-0 -z-50 animate-fade-in "
@@ -25,12 +25,12 @@ export default function Home() {
 
       <div className="relative flex flex-col items-center justify-center w-full h-screen select-none">
         <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-500/50 to-zinc-300/0" />
-        <span className="relative z-10 text-transparent duration-1000 cursor-default text-8xl bg-black/90 text-edge-outline animate-title font-absolut-pro font-bold sm:text-6xl md:text-9xl lg:text-[200px] whitespace-nowrap bg-clip-text -mt-12">
-          <h2 className="absolute top-0 left-0 font-absolut-pro font-bold text-lg lg:text-xl text-start text-zinc-700 animate-fade-in">
+        <span className="relative bg-[url('/black-texture.jpg')] bg-fixed z-10 text-transparent duration-1000 cursor-default text-8xl bg-black/90  animate-title font-absolut-pro font-bold sm:text-6xl md:text-9xl lg:text-[200px] whitespace-nowrap bg-clip-text -mt-12">
+          <h2 className="absolute -top-2 left-1 font-absolut-pro font-bold text-lg lg:text-xl text-start text-zinc-700 animate-fade-in">
             Hello, I&apos;m
           </h2>
-          <h1 className="-ml-6">Leon</h1>
-          <h2 className="absolute top-[100%] right-0 text-lg lg:text-xl text-start text-zinc-700 animate-fade-in">
+          <h1 className="">Leon</h1>
+          <h2 className="absolute top-[100%] right-0 text-lg lg:text-xl text-start text-zinc-800 animate-fade-in">
             Web Developer
           </h2>
         </span>
