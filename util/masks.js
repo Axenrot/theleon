bcdce2cb06c0859fe8a8cbc3f61889fb2f351f42
Leon <yurileon@hotmail.com.br>
2024-02-14 17:56:25 -0300

    mask phone done

--- util/masks.js
+++ util/masks.js
@@ -48,33 +48,14 @@ export const onlyNumbers = (text) => {
   }
 };
 
-export const maskPhone = (text) => {
-  if (!text) {
-    return "";
-  }
+export const maskPhone = (phoneNumber) => {
+  if (!phoneNumber) return "";
 
-  let numbers = onlyNumbers(text.toString());
+  // Remove all non-digit characters from the phone number except common separators
+  const cleanedPhoneNumber = phoneNumber.replace(/[^\d()+\s-]/g, "");
 
-  if (numbers.length < 1) {
-    return "";
-  }
+  // Replace any additional "+" if the user typed it
+  const sanitizedPhoneNumber = cleanedPhoneNumber.replace(/^\+/, "");
 
-  let ddd = numbers.slice(0, 2).join("");
-  let prefix = numbers.slice(2, 6).join("");
-  let sufix = numbers.slice(6, 11).join("");
-
-  if (sufix.length > 0) {
-    prefix = `${prefix}-`;
-  }
-
-  let fone = `(${ddd}) ${prefix}${sufix}`;
-
-  if (sufix.length === 5) {
-    fone = `${fone.slice(0, 9)}${fone.charAt(10)}-${fone.slice(11)}`;
-  }
-  if (text.length == 4) {
-    fone = `(${ddd.slice(0, 1)}`;
-  }
-
-  return fone;
+  return "+" + sanitizedPhoneNumber;
 };
