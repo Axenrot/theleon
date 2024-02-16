7043f0d6dae34c8264510a602c37f4028f5a5806
Leon <yurileon@hotmail.com.br>
2024-02-16 14:14:50 -0300

    refresh on error

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,27 +1,35 @@
 "use client"; // Error components must be Client Components
 
 import { Aperture } from "lucide-react";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function GlobalError({
   error,
-  reset,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
+  const router = useRouter();
+
   useEffect(() => {
     console.error(error);
     if (typeof window !== "undefined") {
       setTimeout(() => {
-        reset();
+        router.push("/");
+        window.location.reload();
       }, 200);
     }
-  }, [error, reset]);
+  }, [error, router]);
 
   return (
     <body className="flex items-center justify-center w-full h-screen overflow-hidden">
-      <span onClick={() => reset()} className="animate-spin w-fit h-fit">
+      <span
+        onClick={() => {
+          router.push("/");
+          window.location.reload();
+        }}
+        className="animate-spin w-fit h-fit"
+      >
         <Aperture size={36} />
       </span>
     </body>
