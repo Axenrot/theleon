a45813cc9d8278c3a00542e7d8960e4f988d507a
Leon <yurileon@hotmail.com.br>
2024-02-16 13:53:17 -0300

    not found fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,33 @@
-"use client"; // Error components must be Client Components
-
-import { Aperture } from "lucide-react";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+"use client";
+// import { useRouter } from "next/router";
+import Link from "next/link";
+import ChromeIcon from "@/components/icons/ChromeIcon";
 
 export default function NotFound() {
-  const router = useRouter();
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setTimeout(() => {
-        router.push("/");
-      }, 200);
-    }
-  }, [router]);
+  // const router = useRouter();
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     setTimeout(() => {
+  //       router.push("/");
+  //     }, 200);
+  //   }
+  // }, [router]);
 
   return (
-    <body className="flex items-center justify-center w-full h-screen overflow-hidden">
-      <span
-        onClick={() => router.push("/")}
+    <body className="flex items-center justify-center gap-3 w-full h-screen overflow-hidden">
+      <h1 className="flex flex-col w-full items-center justify-center font-cold-warm text-4xl">
+        <span>Not found!</span>
+        <span className="font-absolut-pro">
+          Click on the icon below to go home!
+        </span>
+      </h1>
+      <Link
+        // onClick={() => router.push("/")}
+        href={"/"}
         className="animate-spin w-fit h-fit"
       >
-        <Aperture size={36} />
-      </span>
+        <ChromeIcon height={36} width={36} />
+      </Link>
     </body>
   );
 }
