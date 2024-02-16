3cc76e312bc224fb60682a3f9692b5e0d9ef9d43
Leon <yurileon@hotmail.com.br>
2024-02-16 13:36:37 -0300

    error handling

---
+++ app/global-error.tsx
@@ -0,0 +1,27 @@
+"use client"; // Error components must be Client Components
+
+import { Aperture } from "lucide-react";
+import { useEffect } from "react";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+    setTimeout(() => {
+      reset();
+    }, 200);
+  }, [error, reset]);
+
+  return (
+    <body className="flex items-center justify-center w-full h-screen overflow-hidden">
+      <span onClick={() => reset()} className="animate-spin w-fit h-fit">
+        <Aperture size={36} />
+      </span>
+    </body>
+  );
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,25 @@
+"use client"; // Error components must be Client Components
+
+import { Aperture } from "lucide-react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function NotFound() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 200);
+  }, [router]);
+
+  return (
+    <body className="flex items-center justify-center w-full h-screen overflow-hidden">
+      <span
+        onClick={() => router.push("/")}
+        className="animate-spin w-fit h-fit"
+      >
+        <Aperture size={36} />
+      </span>
+    </body>
+  );
+}
